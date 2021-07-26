666d2a9dfb9b0e8f2ab7f0640e90c3df48d00144
imyofather79 <chunsum708@gmail.com>
2021-07-25 18:24:02 -0600

    commit tree 7b1abdab6570f0ec6d3329b387c08f8631149465

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -3,21 +3,28 @@ import whoseHouse from "./houses/whoseHouse";
 import Hufflepuff from "./houses/Hufflepuff";
 import * as GryffFunctions from "./houses/Gryffindor";
 
+
+
 console.log(GryffFunctions.colors);
 // => 'Scarlet and Gold'
 
 GryffFunctions.gryffMascot();
 // => 'The Lion'
 
+GryffFunctions.values();
+
 // GryffFunctions.values();
 // => Attempted import error
 
-export default function Hogwarts() {
+function Hogwarts() {
   whoseHouse();
 
   return (
     <div>
+      <h1>Welcome to Hogwarts!</h1>
       <Hufflepuff />
     </div>
   );
 }
+
+export default Hogwarts;
--- src/houses/whoseHouse.js
+++ src/houses/whoseHouse.js
@@ -1,5 +1,8 @@
+import React from "react";
+
 function whoseHouse() {
   console.log("HAGRID'S HOUSE!");
+  return 
 }
 
 export default whoseHouse;
